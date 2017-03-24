b8e7e42acd3f77aab1f0795816a783d376faad7f
sonal <sonal@buzz4health.com>
2017-03-24 21:23:12 +0530

    Added new asset version && git push heroku master

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,8 @@ Rails.application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
   config.assets.digest = false  
-  
+  config.assets.version = 1.1
+
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
